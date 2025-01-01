469f29eea374000735074981ec50a29e004aca9a
HyperRanger <131896186+HyperRanger@users.noreply.github.com>
2025-01-01 10:16:12 +0100

    Update countdown.js lrtt

--- countdown.js
+++ countdown.js
@@ -1,10 +1,8 @@
 const greetingHeader = document.getElementById('greeting');
-const countdownHeader = document.getElementById('countdown-header');
-const countdownContainer = document.getElementById('countdown-container');
+const header = document.getElementById('header');
+const message = document.getElementById('message');
 const userName = localStorage.getItem('userName');
 
-greetingHeader.innerHTML = `<h1 style="color: #FF69B4; font-size: 60px;">HAPPY NEW YEAR</h1>`;
-countdownHeader.innerHTML = `HAPPY NEW YEAR, ${userName}!`;
+header.innerHTML = `<h1 style="color: #FFD700; font-size: 24px;">HAPPY NEW YEAR, ${userName}!</h1>`;
 
-countdownContainer.innerHTML = `Thank you for an amazing year filled with laughter, memories, and incredible moments. Cheers to an even better year ahead – may it be filled with joy, success, and unforgettable adventures together. ✨<br><br>To ${userName}, a friend who makes life brighter. May the coming year bring us even more laughter, deeper bonds, and countless wonderful memories. Cheers🍻 to a year of amazing friendships!`;
-
+message.innerHTML = `<h1 style="color: #FFFFFF; font-size: 18px;">Thank you for an amazing year filled with laughter, memories, and incredible moments. Cheers to an even better year ahead – may it be filled with joy, success, and unforgettable adventures together. ✨<br><br>To ${userName}, a friend who makes life brighter. May the coming year bring us even more laughter, deeper bonds, and countless wonderful memories. Cheers🍻 to a year of amazing friendships!</h1>`;
